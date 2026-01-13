6a499045ccf565113b0378b95377dbec1063bb03
Rodrigo Núñez <rodrigo.nc2008@gmail.com>
2026-01-13 13:13:21 +0100

    Remove build/deps from repo

--- dist/desarrollador-web.655420d5.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{let e=document.querySelector(".arrow"),t=document.querySelector(".col");e&&t&&t.addEventListener("scroll",()=>{t.scrollTop+t.clientHeight>=t.scrollHeight-70?e.style.opacity="0":e.style.opacity="1"})});
-//# sourceMappingURL=desarrollador-web.655420d5.js.map
--- dist/web.e92f7ae6.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{let e=document.querySelector(".arrow"),t=document.querySelector(".col");e&&t&&t.addEventListener("scroll",()=>{t.scrollTop+t.clientHeight>=t.scrollHeight-70?e.style.opacity="0":e.style.opacity="1"})});
-//# sourceMappingURL=web.e92f7ae6.js.map
